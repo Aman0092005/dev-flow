a2768fcdcdf1c839fd84e0e26fda328cfbb24f33
Aman0092005 <amansinghss009@gmail.com>
2026-06-21 16:33:56 +0530

    Dashboard half part completed

--- dev-flow/src/components/dashboard/Dashboardleft.tsx
+++ dev-flow/src/components/dashboard/Dashboardleft.tsx
@@ -10,22 +10,22 @@ function Dashboardleft()
     return (
         <div className="dashboard-left">
             <Mainlogo />
-            <Servicebutton title="DashBoard" isBackground={true} />
+            <Servicebutton title="DashBoard" isBackground={true} src="./img/dashboard/collapse.png" />
             <div className="main">
                 <h3>MAIN</h3>
-                <Servicebutton title="Workspaces" isBackground={false} />
-                <Servicebutton title="Projects" isBackground={false} />
-                <Servicebutton title="Tasks" isBackground={false} />
-                <Servicebutton title="Notes" isBackground={false} />
-                <Servicebutton title="DSA Tracker" isBackground={false} />
+                <Servicebutton title="Workspaces" isBackground={false} src="./img/dashboard/collapse.png" />
+                <Servicebutton title="Projects" isBackground={false} src="./img/dashboard/collapse.png" />
+                <Servicebutton title="Tasks" isBackground={false} src="./img/dashboard/collapse.png" />
+                <Servicebutton title="Notes" isBackground={false} src="./img/dashboard/collapse.png" />
+                <Servicebutton title="DSA Tracker" isBackground={false} src="./img/dashboard/collapse.png" />
             </div>
             <div className="main">
                 <h3>SETTINGS</h3>
-                <Servicebutton title="Settings" isBackground={false} />
-                <Servicebutton title="Profile" isBackground={false} />
+                <Servicebutton title="Settings" isBackground={false} src="./img/dashboard/collapse.png" />
+                <Servicebutton title="Profile" isBackground={false} src="./img/dashboard/collapse.png" />
             </div>
             <Upgrade />
-            <Servicebutton title="Collapse" isBackground={true} />
+            <Servicebutton title="Collapse" isBackground={true} src="./img/dashboard/collapse.png" />
         </div>
     );
 }
--- dev-flow/src/components/shared/Servicebutton.tsx
+++ dev-flow/src/components/shared/Servicebutton.tsx
@@ -3,15 +3,16 @@
 interface Data{
     title: string;
     isBackground: boolean;
+    src: string;
 }
 
 
 
-function Servicebutton({title, isBackground}: Data)
+function Servicebutton({title, isBackground, src}: Data)
 {
     return (
         <div className={`servicebutton-container ${!isBackground && "background-transparent"}`}>
-            <div className="logo"><img src="./img/home/web.png" alt="Logo image" /></div>
+            <div className="logo"><img src={src} alt="Logo image" /></div>
             <p className="para">{title}</p>
         </div>
     );
